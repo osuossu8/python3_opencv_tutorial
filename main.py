0c8ebf7285b4634a364ec6f49bd29ab7eb71c2c1
osuossu <osuosuossu18@gmail.com>
2018-09-16 01:19:59 +0900

    ok

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import os
+from PIL import Image
+import numpy as np
+import cv2
+from keras.models import load_model
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_world():
+
+    #-- keras が起動出来ているか確認する --#
+    model = load_model('./shiogao_model2.h5')
+    print(model.summary())
+    #-----#
+
+    return "hello world!"
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
